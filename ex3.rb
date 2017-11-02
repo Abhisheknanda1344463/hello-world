12c9710b07be475fe8332294421ec28754d00d0f
Dev (alt) <dev@Mini03s-Mac-mini.local>
2017-11-02 17:19:08 +0530

    exercise 3

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+puts "I will now count my chickens:"
+
+
+puts "hens #{2.5+30/6}"# using for float numbers 
+#Percendence is PEMDAS * / % + -
+
+puts "Roosters #{10.0-25*3%4}"
+
+puts "now i will count the eggs :"
+
+puts 3+2+1-5+4%2-1/4+6 
+
+puts "is It true that 3+2<7-5.0?"
+
+puts 3+2<5-7
+
+puts "what is 3+2? #{3+2}"  #compare and result in boolean true or false
+puts "what is 5-7?#{5-7}"
+
+puts "oh, that's why it's false."
+
+puts "How About Some more."
+
+puts "Is it greater? #{5>-2}"
+puts "IS it greater or equal?#{5>=-2}"
+puts "is it less or equal?#{5<=-2}"
+
+puts "Result of long calcualtion#{343.0122*1235.678}"
