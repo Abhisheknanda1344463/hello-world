07028ebd90f6440205f7b6ab345b60287a2a3bcd
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2017-11-05 02:07:16 +0530

    Exercise 10

---
+++ ex10.rb
@@ -0,0 +1,29 @@
+#study drills 1
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+#study drills2
+puts "It gives us error when we places 3 single quotes instead of"
+puts "Double Quotes becoz in ruby it's not possible  becoz it end ths string at I'll"
+
+#study drills 3
+number1 = 5
+number2 = 2;
+puts "This is a \"double quotes\" within double quotes"
+puts 'this is a \'single quote\' within in single quote'
+puts "Addition of number1 and 2 \n #{number1+number2}"
+puts "for the vertical tab use \v"
+puts "for space \t grass,green"
