532d7fe8a56662626045b7214cf748135c1f1840
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2017-11-04 23:10:33 +0530

    Exercises

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "hello world"
+puts "hello Again"
+puts "I like typing this"
+puts "this is Fun"
+puts "Yay!Printing"
+puts "I'd much rather you 'not'."
+puts 'I "Said" do not touch this.'
---
+++ ex2.rb
@@ -0,0 +1,6 @@
+#A Comment , This is so You can read your program later.
+puts "I could have code like this" #and the comments after is ignored
+
+#puts"This won't run"
+
+puts "this will run"
