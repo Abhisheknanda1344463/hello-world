afdb8ef0bb735c62652346c77207672da90011b3
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2017-11-05 00:42:59 +0530

    Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,22 @@
+#study drills 1
+puts "Mary had a little lamb." # display string
+puts "Its fleece was white as #{'snow'}." #display string with in string as snow also in the single quotes so it treats as string
+puts "And everywhere thst Mary went" #display string
+puts "." * 10 # what'd that do? it printed DOt(.)  10 times
+
+end1 = "C" #store value of String (char) c in the var end1
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6 #use to remain result will be in the same line
+puts end7 + end8 + end9 + end10 + end11 + end12 #display tha additon upto the var end12
+#together form the result cheese burger
