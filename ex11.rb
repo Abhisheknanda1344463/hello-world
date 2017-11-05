010a68d6cb6b3cd4aff238dbcc9b6ddfaf46b5ae
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2017-11-05 23:25:25 +0530

    Exercise 11

--- ex11.rb
+++ ex11.rb
@@ -1,8 +1,23 @@
+#study drills 2
 print "How old are you"
-age = gets.chomp
+age = gets.chomp\
 print "how tall are you"
 height = gets.chomp
-print "how much =do you weigh"
-weight = gets.chomp
+print "how much do you weigh"
+weight = gets.chomp.to_f#it converts weight into floaT As it can also accept float values
 
 puts "So, you're #{age} old ,#{height} tall and #{weight} heavy"
+
+
+#study drills 3
+# my form to ask name birthyear and favnumber n calculates the age
+puts "hello ! what your name?"
+name = gets.chomp
+puts "what is your birthyear ?"
+birthyear = gets.chomp
+age = 2017 -birthyear.to_i
+puts "what is you favourite number?"
+number = gets.chomp
+
+puts """your's name is #{name},your were born in #{birthyear} your's age is #{age}
+and your's favourite number is"""
