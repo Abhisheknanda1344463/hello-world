9e74edc1c8d4b8b035649c923bb2a9b5135a4684
Dev (alt) <dev@173-168-100-49.res.bhn.net>
2017-11-06 18:13:47 +0530

    exercise 19

--- ex19.rb
+++ ex19.rb
@@ -1,5 +1,6 @@
 def cheese_and_crackers(cheese_count,boxes_of_crackers)
-  puts "you have #{cheese_count} cheeses!"
+  # declaring a fn and passing an args cheesecount and box_of cracker
+  puts "you have #{cheese_count} cheeses!"#dispaly stringn interpolation
   puts "you hsvr #{boxes_of_crackers} cheeses!"
   puts "man that's enough for a party!"
   puts "get a blanket"
@@ -7,6 +8,7 @@ end
 
 puts "We can just give the function numbers directly:"
 cheese_and_crackers(20, 30)
+#assign a numeric value arguments
 
 puts "OR, we can use variables from our script:"
 amount_of_cheese = 10
@@ -17,7 +19,8 @@ cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 puts "We can even do math inside too:"
 cheese_and_crackers(10 + 20, 5 + 6)
-
+#parameters value can b calucaltion of any num of values
 
 puts "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+#combination of numerld and varible names
