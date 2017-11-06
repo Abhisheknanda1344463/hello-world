c02cf1ac2e4c99481fbe174ed2f56ca841a2e2bb
Dev (alt) <dev@173-168-100-49.res.bhn.net>
2017-11-06 17:49:08 +0530

    Exrcise 18

---
+++ ex18.rb
@@ -0,0 +1,24 @@
+def print_two(*args)
+  arg1,arg2 = args
+  puts "arg1: #{arg1},arg2: #{arg2}"
+ end
+ 
+ #ok,that *args is actually pointless,we can just do this
+ def print_two_again(arg1,arg2)
+   puts "arg1: #{arg1},arg2: #{arg2}"
+ end
+ 
+ #this is just take one arguments
+ def print_one(arg1)
+   puts "arg1 :#{arg1}"
+ end
+ 
+ #this is one take no arguments
+ def print_none()
+   puts "i got nothin'."
+ end
+ 
+ print_two("Zed","shaw")
+ print_two_again("Zed","shaw")
+ print_one ("First!")
+ print_none()
