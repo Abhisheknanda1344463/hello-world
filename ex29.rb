c7b8fc9b4280c0abb2a120313b0f602f9be87928
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2017-11-07 18:47:48 +0530

    Exercise 29

---
+++ ex29.rb
@@ -0,0 +1,29 @@
+people =  20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "TOO many cats the world is doomed"
+end
+
+if people>cats
+  puts "Not many cats the world is saved"
+end
+
+if people < dogs
+  puts "the world  is drooled on!"
+end
+
+if people > dogs
+puts "the world is dry"
+end
+
+dogs +=5#dog is incremented by 5
+
+if people >=dogs
+  puts "people are less than or equal to dogs"
+end
+
+if people == dogs
+puts "people are dogs"
+end
